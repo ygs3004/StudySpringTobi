1475dddc19b3c3779c62bc1b8f7852af2c8512bc
윤건수 <ygs3004@naver.com>
2022-12-12 03:22:52 +0900

    221212_studying_chap07

--- chap01/src/main/java/springbook/user/sqlservice/before/OxmSqlService7_57.java
+++ chap01/src/main/java/springbook/user/sqlservice/before/OxmSqlService7_57.java
@@ -1,9 +1,8 @@
-package springbook.user.sqlservice;
+package springbook.user.sqlservice.before;
 
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.oxm.Unmarshaller;
 import springbook.user.dao.UserDao;
+import springbook.user.sqlservice.*;
 import springbook.user.sqlservice.jaxb.SqlType;
 import springbook.user.sqlservice.jaxb.Sqlmap;
 
@@ -12,7 +11,7 @@ import javax.xml.transform.Source;
 import javax.xml.transform.stream.StreamSource;
 import java.io.IOException;
 
-public class OxmSqlService implements SqlService {
+public class OxmSqlService7_57 implements SqlService {
 
     private final BaseSqlService baseSqlService = new BaseSqlService();
     private final OxmSqlReader oxmSqlReader = new OxmSqlReader(); // final로 변경 불가능, 강하게 결합
@@ -26,8 +25,8 @@ public class OxmSqlService implements SqlService {
         this.oxmSqlReader.setUnmarshaller(unmarshaller);
     }
 
-    public void setSqlmap(Resource sqlmap){
-        this.oxmSqlReader.setSqlmap(sqlmap);
+    public void setSqlmapFile(String sqlmapFile){
+        this.oxmSqlReader.setSqlmapFile(sqlmapFile);
     }
 
     @PostConstruct
@@ -45,29 +44,29 @@ public class OxmSqlService implements SqlService {
 
     private static class OxmSqlReader implements SqlReader { // 멤버클래스, 탑레벨 클래스인 OxmSqlService만이 사용 가능
 
-        private Resource sqlmap = new ClassPathResource("sqlmap.xml", UserDao.class);
         private Unmarshaller unmarshaller;
+        private final static String DEFAULT_SQLMAP_FILE = "sqlmap.xml";
+        private String sqlmapFile = DEFAULT_SQLMAP_FILE;
 
         public void setUnmarshaller(Unmarshaller unmarshaller) {
             this.unmarshaller = unmarshaller;
         }
 
-        public void setSqlmap(Resource sqlmap) {
-            this.sqlmap = sqlmap;
+        public void setSqlmapFile(String sqlmapFile) {
+            this.sqlmapFile = sqlmapFile;
         }
-
 
         @Override
         public void read(SqlRegistry sqlRegistry) {
             try{
-                Source source = new StreamSource(sqlmap.getInputStream());
+                Source source = new StreamSource(UserDao.class.getResourceAsStream(this.sqlmapFile));
                 Sqlmap sqlmap = (Sqlmap) this.unmarshaller.unmarshal(source); // OxmSqlService 를 통해 전달받은 OXM 인터페이스 구현 오브젝트를 가지고 언마샬링 수행
                 for(SqlType sql : sqlmap.getSql()){
                     sqlRegistry.registerSql(sql.getKey(), sql.getValue());
                 }
 
             }catch (IOException e) {
-                throw new IllegalArgumentException(this.sqlmap.getFilename()+"을 가져올 수 없습니다.");
+                throw new IllegalArgumentException(this.sqlmapFile+"을 가져올 수 없습니다.");
             }
         }
 
