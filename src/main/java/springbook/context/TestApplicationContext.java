57c5cb7da97867bf8446ade5e4fdb71ba6bcdaeb
윤건수 <ygs3004@naver.com>
2023-11-10 10:22:53 +0900

    text-applicationContext -> TestApplicationContext 진행

--- src/main/java/springbook/context/TestApplicationContext.java
+++ src/main/java/springbook/context/TestApplicationContext.java
@@ -1,13 +1,27 @@
 package springbook.context;
 
 import com.mysql.jdbc.Driver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.mail.MailSender;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.transaction.PlatformTransactionManager;
+import springbook.user.dao.UserDao;
+import springbook.user.dao.UserDaoJdbc;
+import springbook.user.service.DummyMailSender;
+import springbook.user.service.UserService;
+import springbook.user.service.UserServiceImpl;
+import springbook.user.service.UserServiceTest;
+import springbook.user.sqlservice.OxmSqlService;
+import springbook.user.sqlservice.SqlRegistry;
+import springbook.user.sqlservice.SqlService;
+import springbook.user.sqlservice.updatable.EmbeddedDbSqlRegistry;
 
+import javax.annotation.Resource;
 import javax.sql.DataSource;
 
 @Configuration
@@ -31,6 +45,61 @@ public class TestApplicationContext {
         DataSourceTransactionManager tm = new DataSourceTransactionManager();
         tm.setDataSource(dataSource());
         return tm;
+    }
+
+    @Bean
+    public UserDao userDao() {
+        UserDaoJdbc dao = new UserDaoJdbc();
+        dao.setDataSource(dataSource());
+        dao.setSqlService(sqlService());
+        return dao;
+    }
+
+    @Bean
+    public UserService userService(){
+        UserServiceImpl service = new UserServiceImpl();
+        service.setUserDao(userDao());
+        service.setMailSender(mailSender());
+        return service;
+    }
+
+    @Bean
+    public UserService testUserService(){
+        UserServiceTest.TestUserService testService = new UserServiceTest.TestUserService();
+        testService.setUserDao(userDao());
+        testService.setMailSender(mailSender());
+        return testService;
+    }
+
+
+    @Bean
+    public MailSender mailSender() {
+        return new DummyMailSender();
+    }
+
+    @Bean
+    public SqlService sqlService(){
+        OxmSqlService sqlService = new OxmSqlService();
+        sqlService.setUnmarshaller(unmarshaller());
+        sqlService.setSqlRegistry(sqlRegistry());
+        return sqlService;
+    }
+
+    @Resource
+    DataSource embeddedDatabase;
+
+    @Bean
+    public SqlRegistry sqlRegistry(){
+        EmbeddedDbSqlRegistry sqlRegistry = new EmbeddedDbSqlRegistry();
+        sqlRegistry.setDataSource(this.embeddedDatabase);
+        return sqlRegistry;
+    }
+
+    @Bean
+    public Jaxb2Marshaller unmarshaller(){
+        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+        marshaller.setContextPath("springbook.user.sqlservice.jaxb");
+        return marshaller;
     }
 
 }
