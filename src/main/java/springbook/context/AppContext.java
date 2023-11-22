687b0f1b00f50558e0f23776f2eccb2ffe4bd8f0
윤건수 <ygs3004@naver.com>
2023-11-23 02:03:36 +0900

    [2023/11/23] SqlMapConfig -> AppText 에서 implements 관리

--- src/main/java/springbook/context/AppContext.java
+++ src/main/java/springbook/context/AppContext.java
@@ -4,18 +4,20 @@ import com.mysql.jdbc.Driver;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import springbook.user.dao.UserDao;
 import springbook.user.service.DummyMailSender;
 import springbook.user.service.UserService;
 import springbook.user.service.UserServiceImpl;
 import springbook.user.service.UserServiceTest;
 import springbook.user.sqlservice.SqlMapConfig;
-import springbook.user.sqlservice.UserSqlMapConfig;
 
 import javax.sql.DataSource;
 
@@ -24,7 +26,7 @@ import javax.sql.DataSource;
 @ComponentScan(basePackages = "springbook.user")
 @Import(SqlServiceContext.class)
 @PropertySource("/database.properties")
-public class AppContext {
+public class AppContext implements SqlMapConfig {
 
     @Value("${db.driverClass}")
     Class<? extends Driver> driverClass;
@@ -69,9 +71,9 @@ public class AppContext {
         return new UserServiceTest.TestUserService();
     }
 
-    @Bean
-    public SqlMapConfig sqlMapConfig(){
-        return new UserSqlMapConfig();
+    @Override
+    public Resource getSqlMapResource() {
+        return new ClassPathResource("sqlmap.xml", UserDao.class);
     }
 
     @Configuration
