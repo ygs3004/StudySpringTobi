76cc2f2a1a5134695366c79fbe86930ebaedbd85
윤건수 <ygs3004@naver.com>
2023-11-29 02:03:46 +0900

    [2023/11/29] 계층 ContextTest

--- src/main/java/springbook/learningtest/spring/ioc/IocTest.java
+++ src/main/java/springbook/learningtest/spring/ioc/IocTest.java
@@ -6,10 +6,14 @@ import org.springframework.beans.factory.config.RuntimeBeanReference;
 import org.springframework.beans.factory.support.PropertiesBeanDefinitionReader;
 import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.context.support.StaticApplicationContext;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.StringUtils;
 import springbook.learningtest.spring.ioc.bean.Hello;
+import springbook.learningtest.spring.ioc.bean.Printer;
 import springbook.learningtest.spring.ioc.bean.StringPrinter;
 
 import static org.hamcrest.core.Is.is;
@@ -93,6 +97,27 @@ public class IocTest {
         hello.print();
 
         assertThat(ac.getBean("printer").toString(), is("Hello Spring"));
+    }
+
+    @Test
+    public void hierarchyContextTest(){
+        String basePath = StringUtils.cleanPath(ClassUtils.classPackageAsResourcePath(getClass()))+ "/";
+        ApplicationContext parent = new GenericXmlApplicationContext(basePath + "parentContext.xml");
+
+        GenericApplicationContext child = new GenericApplicationContext(parent);
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(child);
+        reader.loadBeanDefinitions(basePath + "childContext.xml");
+        // reader 를 사용하여 설정을 읽은경우 refresh() 를 이용하여 초기화
+        child.refresh();
+
+        Printer printer = child.getBean("printer", Printer.class);
+        assertThat(printer, is(notNullValue()));
+
+        Hello hello = child.getBean("hello", Hello.class);
+        assertThat(hello, is(notNullValue()));
+
+        hello.print();
+        assertThat(printer.toString(), is("Hello Child"));
     }
 
 }
