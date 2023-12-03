6ef90fdee16abeccf6d947fa9c718c3b11ef0dca
윤건수 <ygs3004@naver.com>
2023-12-04 03:13:33 +0900

    [2023/12/04] PrototypeBean Test

--- src/main/java/springbook/learningtest/spring/ioc/IocTest.java
+++ src/main/java/springbook/learningtest/spring/ioc/IocTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.context.support.StaticApplicationContext;
@@ -146,13 +147,18 @@ public class IocTest {
     @Test
     public void singletonScope(){
         ApplicationContext ac = new AnnotationConfigApplicationContext(
-                SingletonBean.class, SingletonBean.class
+                SingletonBean.class, SingletonClientBean.class
         );
 
         // Set: 중복값 제거
         Set<SingletonBean> beans = new HashSet<>();
         beans.add(ac.getBean(SingletonBean.class));
         beans.add(ac.getBean(SingletonBean.class));
+
+        assertThat(beans.size(), is(1));
+
+        beans.add(ac.getBean(SingletonClientBean.class).bean1);
+        beans.add(ac.getBean(SingletonClientBean.class).bean2);
 
         assertThat(beans.size(), is(1));
 
@@ -164,5 +170,33 @@ public class IocTest {
         SingletonBean bean1;
         @Autowired
         SingletonBean bean2;
+    }
+
+    @Test
+    public void prototypeScope(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(
+                PrototypeBean.class, PrototypeClientBean.class
+        );
+
+        Set<PrototypeBean> beans = new HashSet<>();
+
+        beans.add(ac.getBean(PrototypeBean.class));
+        assertThat(beans.size(), is(1));
+        beans.add(ac.getBean(PrototypeBean.class));
+        assertThat(beans.size(), is(2));
+        beans.add(ac.getBean(PrototypeClientBean.class).bean1);
+        assertThat(beans.size(), is(3));
+        beans.add(ac.getBean(PrototypeClientBean.class).bean2);
+        assertThat(beans.size(), is(4));
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean{}
+
+    static class PrototypeClientBean{
+        @Autowired
+        PrototypeBean bean1;
+        @Autowired
+        PrototypeBean bean2;
     }
 }
