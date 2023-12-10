dabdeeb34d9a5e2e026025ab6d6bf6dd36c64e39
윤건수 <ygs3004@naver.com>
2023-12-11 00:46:58 +0900

    [2023/12/11] Provider 이용한 DL

--- src/main/java/springbook/learningtest/spring/ioc/controller/ServiceRequestController.java
+++ src/main/java/springbook/learningtest/spring/ioc/controller/ServiceRequestController.java
@@ -1,5 +1,7 @@
 package springbook.learningtest.spring.ioc.controller;
 
+import jakarta.inject.Inject;
+import jakarta.inject.Provider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockHttpServletRequest;
 import springbook.learningtest.spring.ioc.dao.CustomerDao;
@@ -7,18 +9,16 @@ import springbook.learningtest.spring.ioc.dao.ServiceRequestDao;
 import springbook.learningtest.spring.ioc.dto.ServiceRequest;
 import springbook.learningtest.spring.ioc.factory.ServiceRequestFactory;
 
-public abstract class ServiceRequestController {
+public class ServiceRequestController {
 
     CustomerDao customerDao;
     ServiceRequestDao serviceRequestDao;
 
-    abstract public ServiceRequest getServiceRequest();
-
-    @Autowired
-    private ServiceRequestFactory serviceRequestFactory;
+    @Inject
+    Provider<ServiceRequest> serviceRequestProvider;
 
     public void serviceRequestFormSubmit(MockHttpServletRequest request){
-        ServiceRequest serviceRequest = this.getServiceRequest();
+        ServiceRequest serviceRequest = this.serviceRequestProvider.get();
         serviceRequest.setCustomerByCustomerNo(request.getParameter("custNo"));
         this.addNewServiceRequest(serviceRequest);
     }
