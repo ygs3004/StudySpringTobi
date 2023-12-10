2cfc96182d88347283488eeea2cadd863ed2a96c
윤건수 <ygs3004@naver.com>
2023-12-11 00:33:48 +0900

    [2023/12/11] 자체 인터페이스 Factory 이용 DL

--- src/main/java/springbook/learningtest/spring/ioc/controller/ServiceRequestController.java
+++ src/main/java/springbook/learningtest/spring/ioc/controller/ServiceRequestController.java
@@ -1,23 +1,22 @@
 package springbook.learningtest.spring.ioc.controller;
 
-import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockHttpServletRequest;
 import springbook.learningtest.spring.ioc.dao.CustomerDao;
 import springbook.learningtest.spring.ioc.dao.ServiceRequestDao;
 import springbook.learningtest.spring.ioc.dto.ServiceRequest;
-
-import javax.annotation.Resource;
+import springbook.learningtest.spring.ioc.factory.ServiceRequestFactory;
 
 public class ServiceRequestController {
 
     CustomerDao customerDao;
     ServiceRequestDao serviceRequestDao;
 
-    @Resource
-    private ObjectFactory<ServiceRequest> serviceRequestFactory;
+    @Autowired
+    private ServiceRequestFactory serviceRequestFactory;
 
     public void serviceRequestFormSubmit(MockHttpServletRequest request){
-        ServiceRequest serviceRequest = this.serviceRequestFactory.getObject();
+        ServiceRequest serviceRequest = this.serviceRequestFactory.getServiceFactory();
         serviceRequest.setCustomerByCustomerNo(request.getParameter("custNo"));
         this.addNewServiceRequest(serviceRequest);
     }
