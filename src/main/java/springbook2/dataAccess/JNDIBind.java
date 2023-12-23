6e08b0191a0ac155401f77b520acfe9f047dad60
윤건수 <ygs3004@naver.com>
2023-12-24 00:53:07 +0900

    [2023/12/24] JNDI DataSource For Test

---
+++ src/main/java/springbook2/dataAccess/JNDIBind.java
@@ -0,0 +1,17 @@
+package springbook2.dataAccess;
+
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.mock.jndi.SimpleNamingContextBuilder;
+
+import javax.naming.NamingException;
+
+public class JNDIBind {
+
+    public void JNDIBindForTest() throws NamingException {
+        SimpleNamingContextBuilder builder = new SimpleNamingContextBuilder();
+        SimpleDriverDataSource ds = new SimpleDriverDataSource();
+        builder.bind("jdbc/Default", ds);
+        builder.activate();
+    }
+
+}
