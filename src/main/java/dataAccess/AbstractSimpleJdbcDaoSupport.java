e4f28c524c950f72c4df8fc4e13f74a22d283e06
윤건수 <ygs3004@naver.com>
2023-12-25 23:13:32 +0900

    [2023/12/25] JdbcDaoSupport 를 상속한 JdbcDao 생성시 상속받아 사용하는 추상클래스

---
+++ src/main/java/dataAccess/AbstractSimpleJdbcDaoSupport.java
@@ -0,0 +1,19 @@
+package dataAccess;
+
+import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+
+public abstract class AbstractSimpleJdbcDaoSupport extends JdbcDaoSupport {
+
+    protected SimpleJdbcTemplate simpleJdbcTemplate;
+
+    protected void initTemplateConfig(){
+        this.simpleJdbcTemplate = new SimpleJdbcTemplate(getDataSource());
+        initJdbcObjects();
+    }
+
+    // AbstractSimpleJdbcDaoSupport 를 사용받는 경우 오버라이드해서 사용하는 초기화 메서드
+    private void initJdbcObjects() {
+    }
+
+}
