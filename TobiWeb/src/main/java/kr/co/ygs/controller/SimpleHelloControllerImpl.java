e5e7fcc21818434bf5ea453e5a4abc6c874041df
윤건수 <ygs3004@naver.com>
2024-01-04 03:35:42 +0900

    [2023/01/04] Custom Controller Test 를 위한 Handler Mapping

--- TobiWeb/src/main/java/kr/co/ygs/controller/SimpleHelloControllerImpl.java
+++ TobiWeb/src/main/java/kr/co/ygs/controller/SimpleHelloControllerImpl.java
@@ -2,11 +2,15 @@ package kr.co.ygs.controller;
 
 import kr.co.ygs.annotation.RequiredParams;
 import kr.co.ygs.annotation.ViewName;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Map;
 
+@Controller
 public class SimpleHelloControllerImpl implements SimpleController{
 
+    @RequestMapping("/hello")
     @ViewName("/WEB-INF/view/hello.jsp")
     @RequiredParams({"name"})
     public void control(Map<String, String> params, Map<String, Object> model) {
